5211246d1c57cdfda9a9f1d60b5007c81e312376
brilsergei <brilsergei@gmail.com>
2016-09-22 00:26:21 +0300

    Configure multiple drush aliases in php image

--- docker/drupal-php/5.6/default.aliases.drushrc.php
+++ docker/drupal-php/5.6/default.aliases.drushrc.php
@@ -1,6 +1,10 @@
 <?php
 
-$aliases[isset($_SERVER['PHP_SITENAME']) ? $_SERVER['PHP_SITENAME'] : 'dev'] = array(
+$aliases[isset($_SERVER['PHP_SITE_NAME_FT']) ? $_SERVER['PHP_SITE_NAME_FT'] : 'ft'] = array(
   'root' => '/var/www/html/' . (isset($_SERVER['PHP_DOCROOT']) ? $_SERVER['PHP_DOCROOT'] : ''),
-  'uri' => isset($_SERVER['PHP_HOSTNAME']) ? $_SERVER['PHP_HOSTNAME'] : 'localhost:8000',
+  'uri' => isset($_SERVER['PHP_HOST_NAME_FT']) ? $_SERVER['PHP_HOST_NAME_FT'] : 'ft.home:7000',
+);
+$aliases[isset($_SERVER['PHP_SITE_NAME_TAR']) ? $_SERVER['PHP_SITE_NAME_TAR'] : 'tar'] = array(
+  'root' => '/var/www/html/' . (isset($_SERVER['PHP_DOCROOT']) ? $_SERVER['PHP_DOCROOT'] : ''),
+  'uri' => isset($_SERVER['PHP_HOST_NAME_TAR']) ? $_SERVER['PHP_HOST_NAME_TAR'] : 'tar.home:7000',
 );
