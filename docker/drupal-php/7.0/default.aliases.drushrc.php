6e885aa9cf357b6550be732328197fbc0909bc76
brilsergei <brilsergei@gmail.com>
2017-05-25 23:43:37 +0300

    Added drush alias for RT

--- docker/drupal-php/7.0/default.aliases.drushrc.php
+++ docker/drupal-php/7.0/default.aliases.drushrc.php
@@ -8,3 +8,7 @@ $aliases[isset($_SERVER['PHP_SITE_NAME_TT']) ? $_SERVER['PHP_SITE_NAME_TT'] : 't
   'root' => '/var/www/html/' . (isset($_SERVER['PHP_DOCROOT']) ? $_SERVER['PHP_DOCROOT'] : ''),
   'uri' => isset($_SERVER['PHP_HOST_NAME_TT']) ? $_SERVER['PHP_HOST_NAME_TT'] : 'tt.home:8000',
 );
+$aliases[isset($_SERVER['PHP_SITE_NAME_RT']) ? $_SERVER['PHP_SITE_NAME_RT'] : 'rt'] = array(
+  'root' => '/var/www/html/' . (isset($_SERVER['PHP_DOCROOT']) ? $_SERVER['PHP_DOCROOT'] : ''),
+  'uri' => isset($_SERVER['PHP_HOST_NAME_RT']) ? $_SERVER['PHP_HOST_NAME_RT'] : 'rt.home:8000',
+);
